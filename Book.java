3e3da62234fced3a68eaa993815e3a103d0b98fc
yurastepanyuk <yu.stepanyuk.ra@gmail.com>
2015-06-15 14:07:32 +0300

    Add to TEMP

---
+++ Book.java
@@ -0,0 +1,35 @@
+package day4;
+
+public class Book {
+	
+	private String name;
+	private String autor;
+	private Category category;
+		
+	public Book() {
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
+	
+	
+
+}
